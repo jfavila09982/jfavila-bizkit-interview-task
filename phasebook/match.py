0122399707d1d5e453fbe38bab60276666ebb041
unknown <jfavila09982@gmail.com>
2023-02-23 13:11:44 +0800

    semi-completed task

--- phasebook/match.py
+++ phasebook/match.py
@@ -24,4 +24,13 @@ def is_match(fave_numbers_1, fave_numbers_2):
     set2 = set(fave_numbers_2)
 
     return len(set1.intersection(set2))
-    
+    '''
+    Description of refactoring code
+    -----------------------------------------------
+    #This code might takes too long and complexity can be (O*m ) wherein
+    we need to iterate through the length of fave_numbers_2 just to match each number or element. 
+    This would be takes too long if we have large data set or have long lists of favorite numbers.
+    For this given scenario we can use Intersection method to improve the matching algorithm.
+    Since our goal is to only find matching users based on their favorite numbers,  
+    intersection built in method return a set that contains the similarity between sets.
+    '''
